2e38bbae4379a7987fcee162dfabe767ea7229e0
Jason S. Jones <jsjones96@gmail.com>
2021-10-28 17:30:31 -0700

    feat: add nav link component

---
+++ src/modules/orion_labs/navLink/navLink.ts
@@ -0,0 +1,38 @@
+import { LightningElement, api, track, wire } from 'lwc';
+import { CurrentPageReference, NavigationContext, generateUrl, navigate } from 'lwr/navigation';
+import type { ContextId, PageReference } from 'lwr/navigation';
+
+export default class NavLink extends LightningElement {
+    url?: string;
+    @track _isActiveLink: boolean;
+
+    @wire(NavigationContext)
+    navContext?: ContextId;
+
+    _currentPageRef: PageReference;
+    @wire(CurrentPageReference)
+    printPageType(pageRef: PageReference): void {
+        this._currentPageRef = pageRef;
+        this._isActiveLink = this.navigateTo.type === pageRef.type;
+    }
+
+    @api
+    navigateTo: PageReference;
+
+    handleClick(event: Event): void {
+        event.preventDefault();
+        if (this.navContext) {
+            navigate(this.navContext, this.navigateTo);
+        }
+    }
+
+    get computeClass(): string {
+        return `text-2xl hover:text-white ${this._isActiveLink ? 'border-b-2' : ''}`;
+    }
+
+    connectedCallback(): void {
+        if (this.navContext) {
+            this.url = generateUrl(this.navContext, this.navigateTo) || undefined;
+        }
+    }
+}
--- src/modules/orion_labs/navbar/navbar.ts
+++ src/modules/orion_labs/navbar/navbar.ts
@@ -1,12 +1,11 @@
 import { LightningElement, wire } from 'lwc';
 import { NavigationContext, generateUrl, navigate } from 'lwr/navigation';
-import type { ContextId } from 'lwr/navigation';
+import type { ContextId, PageReference } from 'lwr/navigation';
 
 export default class NavBar extends LightningElement {
     @wire(NavigationContext)
     navContext?: ContextId;
     homeUrl?: string;
-    aboutUrl?: string;
 
     handleClick(event: Event): void {
         event.preventDefault();
@@ -17,19 +16,15 @@ export default class NavBar extends LightningElement {
         }
     }
 
-    handleAboutClick(event: Event): void {
-        event.preventDefault();
-        if (this.navContext) {
-            navigate(this.navContext, {
-                type: 'about'
-            });
-        }
+    get aboutPage(): PageReference {
+        return {
+            type: 'about'
+        };
     }
 
     connectedCallback(): void {
         if (this.navContext) {
             this.homeUrl = generateUrl(this.navContext, { type: 'home' }) || undefined;
-            this.aboutUrl = generateUrl(this.navContext, { type: 'about' }) || undefined;
         }
     }
 }
