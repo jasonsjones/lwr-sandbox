b649598a3d528dc5d3587494b9006dc909be2d24
Jason S. Jones <jsjones96@gmail.com>
2021-10-30 13:07:44 -0700

    test: configure project for jest unit tests

---
+++ src/modules/orion_labs/about/__tests__/about.spec.ts
@@ -0,0 +1,19 @@
+import { createElement } from 'lwc';
+import About from 'orion_labs/about';
+
+describe('About component', () => {
+    afterEach(() => {
+        while (document.body.firstChild) {
+            document.body.removeChild(document.body.firstChild);
+        }
+    });
+
+    it('matches snapshot', () => {
+        const element = createElement('orion_labs-about', {
+            is: About
+        });
+        document.body.appendChild(element);
+
+        expect(element).toMatchSnapshot();
+    });
+});
