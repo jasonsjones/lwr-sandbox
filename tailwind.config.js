ffbbd81e842a31e11a7d66242ea4f5ce2d741931
Jason S. Jones <jsjones96@gmail.com>
2021-12-03 16:25:31 -0800

    feat: add mobile nav slide in fm right

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@ module.exports = {
     purge: [],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {}
+        extend: {
+            keyframes: {
+                'slide-in-left': {
+                    '0%': {
+                        opacity: '0',
+                        transform: 'translateX(300px)'
+                    },
+                    '100%': {
+                        opacity: '1',
+                        transform: 'translateX(0)'
+                    }
+                }
+            },
+            animation: {
+                'slide-in-left': 'slide-in-left 0.3s ease-out'
+            }
+        }
     },
     variants: {
         extend: {}
