cdcecf71e9140bb51c847e7cde4b4bba378663ae
Jason S. Jones <jsjones96@gmail.com>
2021-12-06 19:15:31 -0800

    chore: add tailwindcss jit compile for dev flow

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-    purge: [],
+    mode: 'jit',
+    purge: ['./src/modules/orion_labs/**/*.html'],
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {
