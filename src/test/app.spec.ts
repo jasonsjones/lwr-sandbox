319b01a51bbe5dc14f790113c1d2b9323d70d842
Jason S. Jones <jsjones96@gmail.com>
2021-12-07 20:42:07 -0800

    test: add a few simple e2e tests

--- src/test/app.spec.ts
+++ src/test/app.spec.ts
@@ -10,4 +10,22 @@ describe('LWR App', () => {
         const app = await appRoot.getApp();
         expect(await app.isPresent()).toBe(true);
     });
+
+    it('includes an LWR router container', async () => {
+        await browser.url('/');
+
+        const appRoot = await utam.load(AppPage);
+        expect(appRoot instanceof AppPage).toBe(true);
+        const rc = appRoot.getRouterContainer();
+        expect(await (await rc).isPresent()).toBe(true);
+    });
+
+    it('includes a main layout', async () => {
+        await browser.url('/');
+
+        const appRoot = await utam.load(AppPage);
+        expect(appRoot instanceof AppPage).toBe(true);
+        const layout = appRoot.getMainLayout();
+        expect(await (await layout).isPresent()).toBe(true);
+    });
 });
--- utam.config.js
+++ utam.config.js
@@ -1,3 +1,7 @@
 // This project structure follows the default convention.
 // We export an empty object as there's no need to configure the compiler in that case.
-module.exports = {};
+module.exports = {
+    alias: {
+        'lwr-sandbox': '.'
+    }
+};
