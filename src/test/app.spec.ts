bce20674a147ce1d4265aee61ba091cf2b5c698b
Jason S. Jones <jsjones96@gmail.com>
2021-12-08 16:28:59 -0800

    test: add main-layout page object and (simple) tests

--- src/test/app.spec.ts
+++ src/test/app.spec.ts
@@ -1,31 +1,38 @@
 import AppPage from '../../pageObjects/app';
 
-describe('LWR App', () => {
-    it('has top level app custom element', async () => {
+describe('LWR Sandbox App', () => {
+    let appRoot: AppPage;
+
+    beforeEach(async () => {
         await browser.url('/');
-
-        const appRoot = await utam.load(AppPage);
+        appRoot = await utam.load(AppPage);
         expect(appRoot instanceof AppPage).toBe(true);
+    });
 
+    it('has top level app custom element', async () => {
         const app = await appRoot.getApp();
         expect(await app.isPresent()).toBe(true);
     });
 
     it('includes an LWR router container', async () => {
-        await browser.url('/');
-
-        const appRoot = await utam.load(AppPage);
-        expect(appRoot instanceof AppPage).toBe(true);
-        const rc = appRoot.getRouterContainer();
-        expect(await (await rc).isPresent()).toBe(true);
+        const rc = await appRoot.getRouterContainer();
+        expect(await rc.isPresent()).toBe(true);
     });
 
     it('includes a main layout', async () => {
-        await browser.url('/');
+        const layout = await appRoot.getMainLayout();
+        expect(await layout.isPresent()).toBe(true);
+    });
 
-        const appRoot = await utam.load(AppPage);
-        expect(appRoot instanceof AppPage).toBe(true);
-        const layout = appRoot.getMainLayout();
-        expect(await (await layout).isPresent()).toBe(true);
+    it('layout has a navbar', async () => {
+        const layout = await appRoot.getMainLayout();
+        const navbar = await layout.getNavbar();
+        expect(await navbar.isPresent()).toBe(true);
+    });
+
+    it('layout has a footer', async () => {
+        const layout = await appRoot.getMainLayout();
+        const footer = await layout.getFooter();
+        expect(await footer.isPresent()).toBe(true);
     });
 });
