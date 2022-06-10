42e4352f251bd3a9178854428832bdd54dda57c9
Jason Jones <jsjones96@gmail.com>
2022-06-10 11:59:57 -0700

    refactor: separate network adapter

---
+++ src/modules/orion/adapters/adapters.ts
@@ -0,0 +1,1 @@
+export { luvio } from './network';
--- src/modules/orion_labs/userApi/adapters.ts
+++ src/modules/orion_labs/userApi/adapters.ts
@@ -1,35 +1,10 @@
-import { Adapter, Environment, FetchResponse, Luvio, ResourceRequest, Store } from '@luvio/engine';
+import { Adapter } from '@luvio/engine';
+import { luvio } from 'orion/adapters';
 import { createWireAdapterConstructor } from '@luvio/lwc-luvio';
-import { getUsersAdapterFactory } from './generated/adapters/getUsers';
-import { getUserAdapterFactory } from './generated/adapters/getUser';
-import { createUserAdapterFactory } from './generated/adapters/createUser';
+import { getUsersAdapterFactory } from '../../../generated/adapters/getUsers';
+import { getUserAdapterFactory } from '../../../generated/adapters/getUser';
+import { createUserAdapterFactory } from '../../../generated/adapters/createUser';
 
-const store = new Store();
-
-async function networkAdapter(resourceRequest: ResourceRequest): Promise<FetchResponse<any>> {
-    const { baseUri, basePath, body, method } = resourceRequest;
-    const path = `${baseUri}${basePath}`;
-
-    const response = await fetch(path, {
-        method: method.toUpperCase(),
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: body === null ? null : JSON.stringify(body)
-    });
-
-    const resBody = await response.json();
-
-    return {
-        body: resBody,
-        status: response.status,
-        statusText: 'ok',
-        ok: response.status === 200 || response.status === 201,
-        headers: {}
-    };
-}
-
-const luvio = new Luvio(new Environment(store, networkAdapter));
 const getUsersLuvioAdapter = getUsersAdapterFactory(luvio);
 const getUserLuvioAdapter = getUserAdapterFactory(luvio);
 
