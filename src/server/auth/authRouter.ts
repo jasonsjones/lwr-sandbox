f86ec722af49a3af09196032a85fa4a86652f4aa
Jason Jones <jsjones96@gmail.com>
2022-06-28 14:06:39 -0700

    refactor: move authenticate logic to custom middleware

--- src/server/auth/authRouter.ts
+++ src/server/auth/authRouter.ts
@@ -9,7 +9,11 @@ router.get('/sfdc', passport.authenticate('forcedotcom'), () => {
     // callback will not be called.
 });
 
-router.post('/login', passport.authenticate('local'), AuthController.login);
+// Skip using passport-local strategy for now so we can have a bit more control over
+// the shape of the 401 response.  Using custom 'authenticate' middleware.
+
+// router.post('/login', passport.authenticate('local'), AuthController.login);
+router.post('/login', AuthController.authenticate, AuthController.login);
 router.get('/sfdc/callback', passport.authenticate('forcedotcom'), AuthController.sfdcCallback);
 router.get('/me', AuthController.getMe);
 
