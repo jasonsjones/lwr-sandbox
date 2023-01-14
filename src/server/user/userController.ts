82b6856e899564d6c3fdccf921a8ea0324fafeb4
Jason Jones <jsjones96@gmail.com>
2023-01-13 20:07:01 -0800

    fix: set auth user at time of registration

--- src/server/user/userController.ts
+++ src/server/user/userController.ts
@@ -1,10 +1,11 @@
 import { Request, Response } from 'express';
+import { setAuthenticatedUser } from '../auth/authService';
 import * as UserService from './userService';
 
 export async function createUser(req: Request, res: Response) {
     const { firstName, lastName, email, password } = req.body;
     const newUser = await UserService.createUser({ firstName, lastName, email, password });
-
+    setAuthenticatedUser(newUser);
     res.status(201).json(newUser);
 }
 
